2d22743830189f622df5d3eb4c0d248a817df548
samanthamccormick <97063531+samanthamccormick@users.noreply.github.com>
2022-03-18 12:24:07 -0400

    Delete hi directory

--- hi/lib/cl_hmi/src/clcs_hmi_objects.h
+++
@@ -1,7 +0,0 @@
-#include "clcs_hmi_structs.h"
-
-#define TOP_BACK_ID 200
-//buttonSq top_back = {TOP_BACK_ID,0,0,150,80,31, BLUE, "Back"};
-backButton top_back = {TOP_BACK_ID, 0, 0, L_GREY, WHITE};
-
-buttonSq run_settings = {1, 180, 0, 60, 60, 29, B_BLUE, ""};
